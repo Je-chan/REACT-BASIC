97186a985510cd9ecf33362f134dbd268a287044
Je-chan <jkyyc3@gmail.com>
2023-08-21 21:08:23 +0900

    FEAT : 이미지 배너

--- react-disney-plus/src/api/axios.js
+++ react-disney-plus/src/api/axios.js
@@ -1,11 +1,16 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: process.env.AXIOS_BASE_URL,
+  baseURL: process.env.REACT_APP_AXIOS_BASE_URL,
   params: {
-    api_key: process.env.AXIOS_API_KEY,
+    api_key: process.env.REACT_APP_AXIOS_API_KEY,
     language: 'ko-kr',
   },
 });
 
 export default instance;
+
+/**
+ * 동기 : 어떤 요청이 오면 그 요청을 모두 처리한 후, 다음 요청을 처리하는 것
+ * 비동기 : 요청이 들어오면, 아직 응답을 받지 않더라도, 그 다음 요청을 받아서 처리하는 것
+ */
--- react-disney-plus/src/components/Banner.js
+++ react-disney-plus/src/components/Banner.js
@@ -1,7 +1,59 @@
-import React from 'react';
+import axios from '../api/axios';
+import React, { useState, useEffect } from 'react';
+import requests from '../api/request';
+import './Banner.css';
 
 const Banner = () => {
-  return <div></div>;
+  const [movie, setMovie] = useState([]);
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = async () => {
+    const res = await axios.get(requests.fetchNowPlaying);
+
+    const movieId =
+      res.data.results[Math.floor(Math.random() * res.data.results.length)].id;
+
+    const { data: movieDetail } = await axios.get(`movie/${movieId}`, {
+      params: { append_to_response: 'videos' },
+    });
+
+    setMovie(movieDetail);
+  };
+
+  const truncate = (str, n) => {
+    console.log(str);
+    if (str == null || str.length === 0) return '';
+    return str?.length > n ? str.substring(0, n) + '...' : str;
+  };
+
+  return (
+    <header
+      className="banner"
+      style={{
+        backgroundImage: `url('https://image.tmdb.org/t/p/original/${movie.backdrop_path}')`,
+        backgroundPosition: 'top center',
+        backgroundSize: 'cover',
+      }}
+    >
+      <div className="banner__contents">
+        <h1 className="banner__title">
+          {movie.title || movie.name || movie.original_name}
+        </h1>
+
+        <div className="banner__buttons">
+          {movie?.videos?.results[0]?.key && (
+            <button className="banner__button play">Play</button>
+          )}
+        </div>
+
+        <p className="banner__description">{truncate(movie.overview, 100)}</p>
+      </div>
+      <div className="banner-- fadeBottom"></div>
+    </header>
+  );
 };
 
 export default Banner;
