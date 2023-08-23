08eb4bdf115da4a2a4c0469c3b05627278e38c32
Je-chan <jkyyc3@gmail.com>
2023-08-23 22:24:01 +0900

    FEAT : Search

--- react-disney-plus/src/pages/Search/index.js
+++ react-disney-plus/src/pages/Search/index.js
@@ -1,6 +1,33 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useLocation } from 'react-router-dom';
+import axios from '../../api/axios';
 
 const Search = () => {
+  const [searchResults, setSearchResults] = useState([]);
+  const useQuery = () => {
+    return new URLSearchParams(useLocation().search);
+  };
+  let query = useQuery();
+  const searchTerm = query.get('q');
+
+  const fetchSearchMovie = async (searchTerm) => {
+    try {
+      const response = await axios.get(
+        `/search/multi?include_adult=false&query=${searchTerm}`
+      );
+      setSearchResults(response.data.results);
+      console.log('response', response);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    if (searchTerm) {
+      fetchSearchMovie(searchTerm);
+    }
+  }, [searchTerm]);
+
   return <div></div>;
 };
 
