63658ad304c542ca6359a3ecdc11c3f9c24f879f
Je-chan <jkyyc3@gmail.com>
2023-10-07 23:32:11 +0900

    FEAT : Todo List 삭제 로직

--- react-recoil-todo-app/src/components/TodoItem.js
+++ react-recoil-todo-app/src/components/TodoItem.js
@@ -12,9 +12,30 @@ function TodoItem({ item }) {
     });
     setTodoList(newList);
   };
+
+  const toggleItemCompletion = () => {
+    const newList = replaceItemAtIndex(todoList, targetIdx, {
+      ...item,
+      isComplete: !item.isComplete,
+    });
+    setTodoList(newList);
+  };
+
+  const deleteItem = () => {
+    const newList = removeItemAtIndex(todoList, targetIdx);
+
+    setTodoList(newList);
+  };
+
   return (
     <div>
       <input type="text" value={item.text} onChange={editItemText} />
+      <input
+        type="checkbox"
+        checked={item.isComplete}
+        onChange={toggleItemCompletion}
+      />
+      <button onClick={deleteItem}>X</button>
     </div>
   );
 }
@@ -23,4 +44,8 @@ export default TodoItem;
 
 const replaceItemAtIndex = (arr, idx, newValue) => {
   return [...arr.slice(0, idx), newValue, ...arr.slice(idx + 1)];
+};
+
+const removeItemAtIndex = (arr, idx) => {
+  return [...arr.slice(0, idx), ...arr.slice(idx + 1)];
 };
