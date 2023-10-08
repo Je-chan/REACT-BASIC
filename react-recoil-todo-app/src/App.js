e21beded73df6bf047c2244622fa12b263ce73c1
Je-chan <jkyyc3@gmail.com>
2023-10-08 18:29:05 +0900

    FEAT : Todo List 필터와 통계 로직

--- react-recoil-todo-app/src/App.js
+++ react-recoil-todo-app/src/App.js
@@ -1,15 +1,20 @@
-import logo from "./logo.svg";
 import "./App.css";
 import TodoItemCreator from "./components/TodoItemCreator";
 import { useRecoilValue } from "recoil";
-import { todoListState } from "./todoAtoms";
+import { filteredTodoListState, todoListState } from "./todoAtoms";
 import TodoItem from "./components/TodoItem";
+import TodoListFilters from "./components/TodoListFilters";
+import TodoListState from "./components/TodoListState";
 
 function App() {
   const todoList = useRecoilValue(todoListState);
+  const filteredTodoList = useRecoilValue(filteredTodoListState);
   console.log(todoList);
+  console.log(filteredTodoList);
   return (
     <div>
+      <TodoListState />
+      <TodoListFilters />
       <TodoItemCreator />
       {todoList.map((todoItem) => (
         <TodoItem key={todoItem.id} item={todoItem} />
--- react-recoil-todo-app/src/todoAtoms.js
+++ react-recoil-todo-app/src/todoAtoms.js
@@ -1,6 +1,46 @@
-import { atom } from "recoil";
+import { atom, selector } from "recoil";
 
 export const todoListState = atom({
   key: "todoListState",
   default: [],
+});
+
+export const todoListFilterState = atom({
+  key: "todoListFilterState",
+  default: "all",
+});
+
+export const filteredTodoListState = selector({
+  key: "filteredTodoListState",
+  get: ({ get }) => {
+    const filter = get(todoListFilterState);
+    const list = get(todoListState);
+
+    switch (filter) {
+      case "completed":
+        return list.filter((item) => item.isComplete);
+      case "uncompleted":
+        return list.filter((item) => !item.isComplete);
+      default:
+        break;
+    }
+  },
+});
+
+export const todoListStateState = selector({
+  key: "todoListStateState",
+  get: ({ get }) => {
+    const todoList = get(todoListState);
+    const totalNum = todoList.length;
+    const totalCompletedNum = todoList.filter((item) => item.isComplete).length;
+    const totalUncompletedNum = totalNum - totalCompletedNum;
+    const percentCompleted = totalNum === 0 ? 0 : totalCompletedNum / totalNum;
+
+    return {
+      totalNum,
+      totalCompletedNum,
+      totalUncompletedNum,
+      percentCompleted,
+    };
+  },
 });
