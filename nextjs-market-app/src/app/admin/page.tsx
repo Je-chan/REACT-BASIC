c05fb3ef21b5d4e540b235eb1656b1270eaf36ca
Je-chan <jkyyc3@gmail.com>
2023-10-11 22:17:10 +0900

    FEAT : Docker 를 사용한 Postgres DB 생성

---
+++ nextjs-market-app/src/app/admin/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function AdminPage() {
+  return <div>ADMIN</div>;
+}
+
+export default AdminPage;
---
+++ nextjs-market-app/src/app/user/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function UserPage() {
+  return <div>USER PAGE</div>;
+}
+
+export default UserPage;
