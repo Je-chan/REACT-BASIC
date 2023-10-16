973bed9a34fb5072b8ca73222495771f46ea456f
Je-chan <jkyyc3@gmail.com>
2023-10-16 21:02:28 +0900

    FEAT : Auth type 변경

---
+++ nextjs-market-app/types/next-auth.d.ts
@@ -0,0 +1,10 @@
+declare module "next-auth" {
+  import { DefaultSession } from "next-auth";
+
+  interface Session {
+    user?: {
+      id?: string;
+      role?: string;
+    } & DefaultSession["user"];
+  }
+}
