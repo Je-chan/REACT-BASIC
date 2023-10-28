05130b7d3f7af7e3f06538acc812efe24fd3f5aa
Je-chan <jkyyc3@gmail.com>
2023-10-29 00:38:50 +0900

    FEAT : next-cloudinary 를 이용한 이미지 업로드

--- nextjs-market-app/src/app/products/upload/page.tsx
+++ nextjs-market-app/src/app/products/upload/page.tsx
@@ -6,6 +6,7 @@ import { FieldValues, SubmitHandler, useForm } from "react-hook-form";
 import Button from "@/components/Button";
 import Container from "@/components/Container";
 import Heading from "@/components/Heading";
+import ImageUpload from "@/components/ImageUpload";
 
 const ProductUploadPage = () => {
   const [isLoading, setIsLoading] = useState(false);
@@ -29,6 +30,14 @@ const ProductUploadPage = () => {
     },
   });
 
+  // 이렇게 하면, useForm Field 인 imageSrc 가 변경될 때마다 할당해준 변수 imageSrc 의 값이 state 처럼 자동으로 변경된다.
+  const imageSrc = watch("imageSrc");
+
+  // setCustomValue 를 하는 이유는 useFrom 에서 사용하는 register 함수를 사용하고 있지 않기 때문에 사용하는 것
+  const setCustomValue = (id: string, value: any) => {
+    setValue(id, value);
+  };
+
   const onSubmit: SubmitHandler<FieldValues> = (data) => {};
 
   return (
@@ -36,10 +45,10 @@ const ProductUploadPage = () => {
       <div className="max-w-screen-lg mx-auto">
         <form className="flex flex-col gap-8" onSubmit={handleSubmit(onSubmit)}>
           <Heading title="Product Upload" subtitle="upload your product" />
-          {/*<ImageUpload*/}
-          {/*  onChange={(value) => setCustomValue("imageSrc", value)}*/}
-          {/*  value={imageSrc}*/}
-          {/*/>*/}
+          <ImageUpload
+            onChange={(value) => setCustomValue("imageSrc", value)}
+            value={imageSrc}
+          />
           <hr />
 
           <Input
