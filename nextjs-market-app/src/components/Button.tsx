d28b2e99dd4b1e7e634cb92f891e85bfea5e2679
Je-chan <jkyyc3@gmail.com>
2023-10-28 23:50:33 +0900

    FEAT : 상품 업로드 UI 프로토타입

--- nextjs-market-app/src/components/Button.tsx
+++ nextjs-market-app/src/components/Button.tsx
@@ -1,44 +1,65 @@
-import React from "react";
+"use client";
+
 import { IconType } from "react-icons";
+import { RotatingLines } from "react-loader-spinner";
+
 interface ButtonProps {
-  label: string;
-  onClick: (e: React.MouseEvent<HTMLButtonElement>) => void;
+  label?: string;
+  onClick?: (e: React.MouseEvent<HTMLButtonElement>) => void;
   disabled?: boolean;
   outline?: boolean;
   small?: boolean;
-  Icon?: IconType;
+  icon?: IconType;
+  isLoading?: boolean;
 }
-const Button = ({
+
+const Button: React.FC<ButtonProps> = ({
   label,
   onClick,
   disabled,
   outline,
   small,
-  Icon,
-}: ButtonProps) => {
+  icon: Icon,
+  isLoading = false,
+}) => {
   return (
     <button
-      type={"submit"}
+      type="submit"
       disabled={disabled}
       onClick={onClick}
       className={`
-      reactive
-      disabled:opacity-70
-      rounded-lg
-      hover:opacity-80
-      transition
-      w-full
-      ${outline ? "bg-white" : "bg-orange-500"}
-      ${outline ? "border-black" : "border-orange-500"}
-      ${outline ? "text-black" : "text-white"}
-      ${small ? "text-sm" : "text-md"}
-      ${small ? "py-1" : "py-3"}
-      ${small ? "font-light" : "font-semibold"}
-      ${small ? "border-[1px]" : "border-2"}
-    `}
+        relative
+        disabled:opacity-70
+        disabled:cursor-not-allowed
+        rounded-lg
+        hover:opacity-80
+        transition   
+        w-full
+        ${outline ? "bg-white" : "bg-orange-500"}
+        ${outline ? "border-black" : "border-orange-500"}
+        ${outline ? "text-black" : "text-white"}
+        ${small ? "text-sm" : "text-md"}
+        ${small ? "py-1" : "py-3"}
+        ${small ? "font-light" : "font-semibold"}
+        ${small ? "border-[1px]" : "border-2"}
+      `}
     >
-      {Icon && <Icon size={24} className={"absolute left-4 top-3"} />}
-      {label}
+      {isLoading ? (
+        <div className="flex justify-center">
+          <RotatingLines
+            strokeColor="grey"
+            strokeWidth="5"
+            animationDuration="0.75"
+            width="30"
+            visible={true}
+          />
+        </div>
+      ) : (
+        <>
+          {Icon && <Icon size={24} className="absolute left-4 top-3" />}
+          {label && label}
+        </>
+      )}
     </button>
   );
 };
