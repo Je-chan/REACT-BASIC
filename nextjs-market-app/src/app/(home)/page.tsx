c9809922e0face43927cdf21a8727ae4cd0c7d36
Je-chan <jkyyc3@gmail.com>
2023-12-25 00:14:35 +0900

    FEAT : ProductCard

--- nextjs-market-app/src/app/(home)/page.tsx
+++ nextjs-market-app/src/app/(home)/page.tsx
@@ -1,4 +1,9 @@
 import getProducts, { ProductsParams } from "@/app/actions/getProduct";
+import Container from "@/components/Container";
+import EmptyState from "@/components/EmptyState";
+import ProductCard from "@/components/products/ProductCard";
+import getCurrentUser from "@/app/actions/getCurrentUser";
+import FloatingButton from "@/components/FloatingButton";
 
 interface HomeProps {
   searchParams: ProductsParams;
@@ -6,6 +11,32 @@ interface HomeProps {
 
 export default async function Home({ searchParams }: HomeProps) {
   const products = await getProducts(searchParams);
+  const currentUser = await getCurrentUser();
+
   console.log(products);
-  return <main>누구나 볼 수 있는 페이지입니다.</main>;
+  return (
+    <Container>
+      {/*  Category */}
+
+      {/*  Products */}
+
+      {products?.data.length === 0 ? (
+        <EmptyState />
+      ) : (
+        <>
+          <div className="grid grid-cols-1 gap-8 pt-12 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6">
+            {products.data.map((product) => (
+              <ProductCard
+                currentUser={currentUser}
+                key={product.id}
+                data={product}
+              />
+            ))}
+          </div>
+
+          <FloatingButton href="/products/upload">+</FloatingButton>
+        </>
+      )}
+    </Container>
+  );
 }
