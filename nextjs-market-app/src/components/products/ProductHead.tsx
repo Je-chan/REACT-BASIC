ce1dbdde5d14b81d6df9cfadef546864b80af1ea
Je-chan <jkyyc3@gmail.com>
2024-01-06 23:31:25 +0900

    FEAT : 상품 상세 페이지 Heading 구성

--- nextjs-market-app/src/components/products/ProductHead.tsx
+++ nextjs-market-app/src/components/products/ProductHead.tsx
@@ -1,7 +1,45 @@
 import React from "react";
+import Heading from "@/components/Heading";
+import Image from "next/image";
+import HeartButton from "@/components/HeartButton";
+import { User } from "@prisma/client";
 
-const ProductHead = () => {
-  return <div>Product Head</div>;
+interface ProductHeadProps {
+  title: string;
+  imageSrc: string;
+  id: string;
+  currentUser?: User | null;
+}
+const ProductHead = ({
+  title,
+  imageSrc,
+  id,
+  currentUser,
+}: ProductHeadProps) => {
+  return (
+    <>
+      <Heading title={title} />
+      <div
+        className="
+          w-full
+          h-[60vh]
+          overflow-hidden
+          rounded-xl
+          relative
+        "
+      >
+        <Image
+          src={imageSrc}
+          alt={"product"}
+          fill
+          className={"object-cover w-full"}
+        />
+        <div className="absolute top-5 right-5">
+          <HeartButton productId={id} currentUser={currentUser} />
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default ProductHead;
