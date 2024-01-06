b21f69842765a8838cb38be66a0b82a0d61c7cd8
Je-chan <jkyyc3@gmail.com>
2024-01-06 23:27:49 +0900

    FEAT : 상품 상세 페이지 Layout 및 하위 컴포넌트 구성

--- nextjs-market-app/src/app/products/[productId]/ProductClient.tsx
+++ nextjs-market-app/src/app/products/[productId]/ProductClient.tsx
@@ -2,6 +2,12 @@
 
 import React from "react";
 import { Product, User } from "@prisma/client";
+import Container from "@/components/Container";
+import ProductHead from "@/components/products/ProductHead";
+import ProductInfo from "@/components/products/ProductInfo";
+import dynamic from "next/dynamic";
+import Button from "@/components/Button";
+import { useRouter } from "next/navigation";
 
 interface ProductClientProps {
   product: Product & { user: User };
@@ -9,7 +15,37 @@ interface ProductClientProps {
 }
 
 const ProductClient = ({ product, currentUser }: ProductClientProps) => {
-  return <div>PRODUCT CLIENT</div>;
+  const router = useRouter();
+
+  const KakaoMap = dynamic(() => import("../../../components/KakaoMap"), {
+    ssr: false,
+  });
+
+  return (
+    <Container>
+      <div className="max-w-screen-lg mx-auto ">
+        <div className="flex flex-col gap-6">
+          <ProductHead />
+          <div className="grid grid-cols-1 mt-6 md:grid-cols-7 md:gap-10">
+            <ProductInfo />
+            <div className="order-first mb-10 md:order-last md:col-span-3">
+              <KakaoMap
+                detailPage
+                latitude={product.latitude}
+                longitude={product.longitude}
+              />
+            </div>
+          </div>
+        </div>
+        <div>
+          <Button
+            label={"이 유저와 채팅하기"}
+            onClick={() => router.push("/chat")}
+          />
+        </div>
+      </div>
+    </Container>
+  );
 };
 
 export default ProductClient;
