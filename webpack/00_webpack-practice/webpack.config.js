edf2a73caf8136af1d8e5ea5a25f0dbd9e6aebb7
ye chan <jkyyc3@gmail.com>
2024-07-03 23:39:02 +0900

    feat: webpack caching

--- webpack/00_webpack-practice/webpack.config.js
+++ webpack/00_webpack-practice/webpack.config.js
@@ -6,7 +6,8 @@ module.exports = {
   entry: path.resolve(__dirname, "src/index.js"),
   output: {
     path: path.resolve(__dirname, "dist"),
-    filename: "main.js",
+    filename: "[name][contenthash].js",
+    clean: true,
   },
   module: {
     rules: [
