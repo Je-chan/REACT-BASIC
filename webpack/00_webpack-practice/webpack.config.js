aca7900b72decd041e2f7138b34c01bd7bc319dd
ye chan <jkyyc3@gmail.com>
2024-07-04 23:59:43 +0900

    feat: webpack dev

--- webpack/00_webpack-practice/webpack.config.js
+++ webpack/00_webpack-practice/webpack.config.js
@@ -23,4 +23,12 @@ module.exports = {
       template: "src/index.html",
     }),
   ],
+  devServer: {
+    static: {
+      directory: path.join(__dirname, "dist"),
+    },
+    compress: true,
+    port: 3010,
+    open: true,
+  },
 };
